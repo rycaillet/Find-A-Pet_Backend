6bb3a4809076f598f26815a2e69e28aa284daf12
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-12-08 15:52:58 -0500

    added striptoken middleware

--- middleware/index.js
+++ middleware/index.js
@@ -30,3 +30,15 @@ const verifyToken = (req, res, next) => {
   }
   res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
 }
+
+const stripToken = (req, res, next) => {
+  try {
+    const token = req.headers['authorization'].split(' ')[1]
+    if (token) {
+      res.locals.token = token
+      return next()
+    }
+  } catch (error) {
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  }
+}
