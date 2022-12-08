5ad1a928686e0681c8f32cbf9e57ceb53f31d3b0
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-12-08 16:03:50 -0500

    added a login and register function for auth controller

---
+++ controller/AuthController.js
@@ -0,0 +1,36 @@
+const { User } = require('../models')
+const middleware = require('../middleware')
+
+const Login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { userName: req.body.userName },
+      raw: true
+    })
+    if (
+      user &&
+      (await middleware.comparePassword(user.passwordDigest, req.body.password))
+    ) {
+      let payload = {
+        id: user.id,
+        userName: user.userName
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    throw error
+  }
+}
+
+const Register = async (req, res) => {
+  try {
+    const { userName, password } = req.body
+    let passwordDigest = await middleware.hashPassword(password)
+    const user = await User.create({ userName, passwordDigest })
+    res.send(user)
+  } catch (error) {
+    throw error
+  }
+}
