6304b49d0cf3e8a6719bf2398c4bbf9d64980938
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-12-08 12:20:31 -0500

    added middleware folder and controller folder with files

--- models/listing.js
+++ models/listing.js
@@ -2,11 +2,6 @@
 const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Listing extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Listing.hasMany(models.Comment, {
         foreignKey: 'listingId',
