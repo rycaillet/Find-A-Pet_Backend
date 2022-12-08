252db235514e88e5fff812c22d983046ae6fa92b
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-12-08 15:50:27 -0500

    added verifytoken middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const { nextTick } = require('process')
 require('dotenv').config()
 
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
@@ -18,4 +19,14 @@ const comparePassword = async (storedPassword, password) => {
 const createToken = (payload) => {
   let token = jwt.sign(payload, APP_SECRET)
   return token
+}
+
+const verifyToken = (req, res, next) => {
+  const { token } = res.locals
+  let payload = jwt.verify(token, APP_SECRET)
+  if (payload) {
+    res.locals.payload = payload
+    return next()
+  }
+  res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
 }
